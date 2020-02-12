51f4b2fb7539db6c436e74c6515a76837ce1adcd
iamrames <addictedtomig@gmail.com>
2020-02-12 20:00:51 +0545

    Intial Commit

---
+++ ExploreNepal/ViewComponents/MonthlySpecialsViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExploreNepal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreNepal.ViewComponents
+{
+    [ViewComponent]
+    public class MonthlySpecialsViewComponent : ViewComponent
+    {
+        private readonly BlogDataContext db;
+
+        public MonthlySpecialsViewComponent(BlogDataContext db)
+        {
+            this.db = db;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var specials = db.MonthlySpecials.ToArray();
+            return View(specials);
+        }
+
+    }
+}
